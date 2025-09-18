d5041fc1cacd581eec9bd26f152f3c898ea4326d
Aqib Khan <aqibkhan1582000@gmail.com>
2025-09-18 23:32:22 +0500

    push

--- base/views.py
+++ base/views.py
@@ -131,6 +131,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
+            room = form.save(commit = False)
+            room.host = request.user
+            room.save()
             form.save()
             return redirect('home')
     context = {'form':form}
